a54868fbecaa23fb59f0f92c71aa28739af5681c
Michael Lynch <lynchmv@gmail.com>
2025-03-27 17:13:13 -0400

    Moved logging from file based to dictConfig

--- src/main.py
+++ src/main.py
@@ -8,26 +8,15 @@ from api.utr_api import UTRAPI
 from processing.data_saver import save_player_profile, save_player_results, save_player_stats
 from analytics.utr_service import get_player_utr_scores # Import the UTR calculation function
 
-DEBUG_MODE = False
-
 def main():
-    global DEBUG_MODE
     parser = argparse.ArgumentParser(description="UTR Player Lookup CLI")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging")
     parser.add_argument("--player", "-p", help="Player name to search")
     args = parser.parse_args()
 
-    DEBUG_MODE = args.debug
-
-    logging.config.fileConfig('logging.conf')
-    root_logger = logging.getLogger()
-    root_logger.debug("Debug message from root logger in main.py")
-
-    logger = logging.getLogger('application')
-
-    if DEBUG_MODE:
-        logger.setLevel(logging.DEBUG)
-        logger.debug("Debug mode enabled for application logger.")
+    configure_logging(debug_mode=args.debug)
+    logger = logging.getLogger("application")
+    logger.debug("Debug mode enabled for application logger.")
 
     api = UTRAPI()
 
@@ -95,6 +84,99 @@ def main():
         else:
             print("\nNo UTR scores found.")
         print("-" * 40)
+
+def configure_logging(debug_mode=False):
+    """Configures logging dynamically using dictConfig with separate log files."""
+    log_level = logging.DEBUG if debug_mode else logging.INFO
+
+    log_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "%(levelname)s - %(message)s",
+            },
+            "detailed": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": log_level,
+                "formatter": "simple",
+                "stream": sys.stdout,
+            },
+            "application_file": {
+                "class": "logging.FileHandler",
+                "level": log_level,
+                "formatter": "detailed",
+                "filename": "application.log",
+                "mode": "a",
+            },
+            "api_file": {
+                "class": "logging.FileHandler",
+                "level": log_level,
+                "formatter": "detailed",
+                "filename": "api.log",
+                "mode": "a",
+            },
+            "analytics_file": {
+                "class": "logging.FileHandler",
+                "level": log_level,
+                "formatter": "detailed",
+                "filename": "analytics.log",
+                "mode": "a",
+            },
+            "data_access_file": {
+                "class": "logging.FileHandler",
+                "level": log_level,
+                "formatter": "detailed",
+                "filename": "data_access.log",
+                "mode": "a",
+            },
+            "processing_file": {
+                "class": "logging.FileHandler",
+                "level": log_level,
+                "formatter": "detailed",
+                "filename": "processing.log",
+                "mode": "a",
+            },
+        },
+        "loggers": {
+            "application": {
+                "level": log_level,
+                "handlers": ["application_file"],
+                "propagate": False,
+            },
+            "analytics": {
+                "level": log_level,
+                "handlers": ["analytics_file"],
+                "propagate": False,
+            },
+            "api": {
+                "level": log_level,
+                "handlers": ["api_file"],
+                "propagate": False,
+            },
+            "data_access": {
+                "level": log_level,
+                "handlers": ["data_access_file"],
+                "propagate": False,
+            },
+            "processing": {
+                "level": log_level,
+                "handlers": ["processing_file"],
+                "propagate": False,
+            },
+        },
+        "root": {
+            "level": log_level,
+            "handlers": ["console", "application_file"],
+        },
+    }
+
+    logging.config.dictConfig(log_config)
 
 if __name__ == "__main__":
     main()
